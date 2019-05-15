235eca4588b602101405f7ecb0d9ce3553d1a13e
IwonaWilk <iwona.k.bialecka@gmail.com>
2019-05-15 21:32:01 +0100

    validations for picture

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,6 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, uniqueness: true, presence: true
+  validates :picture, presence: true
   mount_uploader :picture, PictureUploader
 end
